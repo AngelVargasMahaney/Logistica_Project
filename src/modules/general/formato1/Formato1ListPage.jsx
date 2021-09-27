75f3b57f57f1cdf7194c29e957f1954a6a07eb55
Angel <angelvgmh@gmail.com>
2021-09-27 09:00:49 -0500

    Version 3

--- src/modules/general/formato1/Formato1ListPage.jsx
+++ src/modules/general/formato1/Formato1ListPage.jsx
@@ -10,8 +10,11 @@ import GeneralNavBar from "../../layout/GeneralNavBar";
 import Swal from "sweetalert2";
 import Modal from "react-bootstrap/Modal";
 import { Button } from "react-bootstrap";
-import { postInternarBienFormato1 } from "../../../services/internamientoFormato1Service";
-
+import { postInternarBienFormato1, postReasignarBienFormato1 } from "../../../services/internamientoFormato1Service";
+import { getSubunidades } from "../../../services/subunidadesService";
+import { getPersonal } from "../../../services/personalService";
+import { useParams } from 'react-router-dom'
+import { getHistorialFormatoById } from "../../../services/historialBienesService";
 const Formato1ListPage = () => {
   const urlFormatoCrear = "/admin/formatos/crear";
   const [formatos, setFormatos] = useState([]);
@@ -26,15 +29,20 @@ const Formato1ListPage = () => {
     documento_acta_entrega_recepcion: "",
     documento_oficio_regularizacion: "",
     bien_id: "",
-    tipo_bien: 1
+    tipo_bien: 1,
+    area_oficina_seccion_id: "",
+    personal_id: ""
   })
 
-  let { estado_del_bien, fecha, observaciones, bien_id, tipo_bien } = formulario
+  let { estado_del_bien, fecha, observaciones, bien_id, tipo_bien, area_oficina_seccion_id,
+    personal_id } = formulario
 
   const [showModall, setShow] = useState(false);
+  const [showModalReasignar, setshowModalReasignar] = useState(false);
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
+  const handleCloseReasignar = () => setshowModalReasignar(false);
+  const handleShowModalInternarBien = () => setShow(true);
+  const handleShowModalReasignarBien = () => setshowModalReasignar(true);
 
   const [documentoRecepcion, setDocumentoRecepcion] = useState(null)
   const [documentoRegularizacion, setDocumentoRegularizacion] = useState(null)
@@ -53,6 +61,14 @@ const Formato1ListPage = () => {
       [e.target.name]: e.target.value
     })
   }
+  const token = localStorage.getItem('token')
+
+  const config = {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+      'Authorization': `Bearer ${token}`
+    }
+  }
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -66,14 +82,10 @@ const Formato1ListPage = () => {
     formData.append('documento_oficio_regularizacion', documentoRegularizacion)
     formData.append('bien_id', formulario.bien_id)
     formData.append('tipo_bien', formulario.tipo_bien)
-    const token = localStorage.getItem('token')
 
-    const config = {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-        'Authorization': `Bearer ${token}`
-      }
-    }
+
+
+
     postInternarBienFormato1(formData, config).then((rpta) => {
       if (rpta.status === 200) { //Si el status es OK, entonces redirecciono a la lista de usuarios
         console.log("Datos subida correctamente")
@@ -94,8 +106,42 @@ const Formato1ListPage = () => {
       )
     })
 
+  }
 
 
+  const handleSubmitReasignacion = e => {
+    e.preventDefault();
+    const formDataReasignacion = new FormData();
+    formDataReasignacion.append('estado_del_bien', formulario.estado_del_bien)
+    formDataReasignacion.append('fecha:', formulario.fecha)
+    formDataReasignacion.append('observaciones', formulario.observaciones)
+    formDataReasignacion.append('documento_acta_entrega_recepcion', documentoRecepcion)
+    formDataReasignacion.append('documento_oficio_regularizacion', documentoRegularizacion)
+    formDataReasignacion.append('bien_id', formulario.bien_id)
+    formDataReasignacion.append('tipo_bien', formulario.tipo_bien)
+    formDataReasignacion.append('area_oficina_seccion_id', formulario.area_oficina_seccion_id)
+    formDataReasignacion.append('personal_id', formulario.personal_id)
+
+
+    postReasignarBienFormato1(formDataReasignacion, config).then((rpta) => {
+      if (rpta.status === 200) { //Si el status es OK, entonces redirecciono a la lista de usuarios
+        console.log("Datos subida correctamente")
+        Swal.fire(
+          'Reasignación Exitosa',
+          'La reasignación fue exitoso',
+          'success'
+        )
+        traerFormatos()
+      }
+
+      console.log(rpta)
+    }).catch((err) => {
+      Swal.fire(
+        'Reasignaciòn Fallida',
+        'No se puede internar un bien dos veces',
+        'error'
+      )
+    })
   }
 
   // Aqui acaban los metodos para el modal de internamiento
@@ -142,6 +188,48 @@ const Formato1ListPage = () => {
     });
   };
 
+  //Aqui los metodos para traer las subunidades
+  const [subunidades, setSubunidades] = useState([]);
+  const traerSubunidades = () => {
+    getSubunidades().then((rpta) => {
+      console.log(rpta);
+      setSubunidades(rpta.data);
+    });
+  };
+  useEffect(() => {
+    traerSubunidades();
+  }, []);
+  //Aqui los metodos para traer las personales
+  const [personal, setPersonal] = useState([]);
+  const traerPersonal = () => {
+    getPersonal().then((rpta) => {
+      console.log(rpta);
+      setPersonal(rpta.data);
+    });
+  };
+  useEffect(() => {
+    traerPersonal();
+  }, []);
+
+  // Metodos para traer el historial
+  const params = useParams()
+  const [dataHistorial, setDataHistorial] = useState([])
+
+  const traerDataHistorial = () => {
+    setCargando(true)
+    const idUrl = params.id;
+    getHistorialFormatoById(idUrl).then(rpta => {
+      setDataHistorial(rpta.data);
+      setCargando(false)
+      
+    })
+  }
+  useEffect(() => {
+    traerDataHistorial();
+    // eslint-disable-next-line
+}, [])
+
+console.log(dataHistorial)
   return (
     <>
       <AdminSidebar />
@@ -153,6 +241,11 @@ const Formato1ListPage = () => {
               <div className="card-body">
                 <div className="d-flex justify-content-between mb-3">
                   <h5>Lista de Bienes del Formato 1</h5>
+                  <Link to="/admin/bienes-internados/formato1" className="btn btn-warning">
+                    {" "}
+                    <i className="fa fa-list"></i> Lista de Bienes Internados
+                  </Link>
+
                   <Link to={urlFormatoCrear} className="btn btn-primary ">
                     {" "}
                     <i className="fa fa-plus"></i> Crear un Bien
@@ -194,9 +287,7 @@ const Formato1ListPage = () => {
                                 <th>Forma_adquisicion</th>
                                 <th>Observaciones</th>
                                 <th>Imagen_bien</th>
-                                <th>Deleted_at</th>
-                                <th>Created_at</th>
-                                <th>Updated_at</th>
+
 
                                 <th className="acciones">Acciones</th>
                               </tr>
@@ -267,9 +358,7 @@ const Formato1ListPage = () => {
                                         }
                                       />
                                     </td>
-                                    <td>{objFormato.deleted_at}</td>
-                                    <td>{objFormato.created_at}</td>
-                                    <td>{objFormato.updated_at}</td>
+
 
                                     <td>
                                       <button
@@ -286,9 +375,29 @@ const Formato1ListPage = () => {
                                       <Link
                                         to={`formatos/editar/${objFormato.id}`}
                                         className="btn btn-warning"
+                                        title="Modificar"
                                       >
                                         {" "}
                                         <i className="fa fa-pencil"></i>
+                                      </Link>
+
+                                      <Link
+                                        // to={`formatos/editar/${objFormato.id}`}
+                                        to={`/admin/formato1/historial/${objFormato.id}`}
+                                        className="btn btn-info ml-1"
+                                        title="Historial del bien"
+                                      >
+                                        {" "}
+                                        <i className="fa fa-history"></i>
+                                      </Link>
+
+                                      <Link
+                                        to={`/admin/formato1/historial/${objFormato.id}`}
+                                        className="btn btn-info mx-1"
+                                        title="Historial del bien"
+                                      >
+                                        {" "}
+                                        <i className="fas fa-clipboard-check"></i>
                                       </Link>
 
                                     </td>
@@ -302,8 +411,66 @@ const Formato1ListPage = () => {
                   </div>
                 </div>
 
-                <button type="button" className="btn btn-primary btn-lg btn-block mt-5" onClick={handleShow}>Internar un Bien</button>
-                <Link to="/admin/bienes-internados/formato1" className="btn btn-warning btn-lg btn-block mt-5">Ver Lista de Bienes Internados</Link>
+                <button type="button" className="btn btn-primary btn-lg btn-block mt-5 mb-5" onClick={handleShowModalInternarBien}>Internar un Bien</button>
+                <button type="button" className="btn btn-dark btn-lg btn-block mt-5 mb-5" onClick={handleShowModalReasignarBien}>Reasignar un Bien</button>
+
+                <Modal show={showModalReasignar} onHide={handleCloseReasignar}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Internamiento de un bien del Formato 1</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                    <form onSubmit={handleSubmit}>
+                      <div className="form-group">
+                        <label htmlFor="">Estado del Bien:</label>
+                        <input type="text" className="form-control"
+                          value={estado_del_bien} name="estado_del_bien" onChange={handleChange} />
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="">Fecha:</label>
+                        <input type="date" className="form-control"
+                          value={fecha} name="fecha" onChange={handleChange} />
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="">Observaciones:</label>
+                        <input type="text" className="form-control"
+                          value={observaciones} name="observaciones" onChange={handleChange} />
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="">Documento-Acta entrega y recepción:</label>
+                        <input type="file" className="form-control"
+                          name="documento_acta_entrega_recepcion" onChange={handleDocumentRecepcion} />
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="">Documento-Oficio regularización:</label>
+                        <input type="file" className="form-control"
+                          name="documento_oficio_regularizacion" onChange={handleDocumentRegularizacion} />
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="">Id del Bien:</label>
+                        <input type="text" className="form-control"
+                          value={bien_id} name="bien_id" onChange={handleChange} />
+                      </div>
+                      {/* <div className="form-group">
+                        <label htmlFor="">Tipo bien</label>
+                        <input type="text" className="form-control"
+                          value={tipo_bien} name="tipo_bien" onChange={handleChange} />
+                      </div> */}
+
+                      <div className="form-group">
+                        <button className="btn btn-primary" type="submit">Internar</button>
+                      </div>
+
+
+                    </form>
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseReasignar}>
+                      Cerrar
+                    </Button>
+
+                  </Modal.Footer>
+                </Modal>
+
 
 
                 <Modal show={showModall} onHide={handleClose}>
@@ -362,6 +529,91 @@ const Formato1ListPage = () => {
 
                   </Modal.Footer>
                 </Modal>
+                {/* Modal de Reasignar un Bien */}
+                <Modal show={showModalReasignar} onHide={handleCloseReasignar}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Reasignación de un bien del Formato 1</Modal.Title>
+                  </Modal.Header>
+                 
+                  <Modal.Body>
+                    <form onSubmit={handleSubmitReasignacion}>
+                      <div className="form-group">
+                        <label htmlFor="">Estado del Bien:</label>
+                        <input type="text" className="form-control"
+                          value={estado_del_bien} name="estado_del_bien" onChange={handleChange} />
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="">Fecha:</label>
+                        <input type="date" className="form-control"
+                          value={fecha} name="fecha" onChange={handleChange} />
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="">Observaciones:</label>
+                        <input type="text" className="form-control"
+                          value={observaciones} name="observaciones" onChange={handleChange} />
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="">Documento-Acta entrega y recepción:</label>
+                        <input type="file" className="form-control"
+                          name="documento_acta_entrega_recepcion" onChange={handleDocumentRecepcion} />
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="">Documento-Oficio regularización:</label>
+                        <input type="file" className="form-control"
+                          name="documento_oficio_regularizacion" onChange={handleDocumentRegularizacion} />
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="">Id del Bien:</label>
+                        <input type="text" className="form-control"
+                          value={bien_id} name="bien_id" onChange={handleChange} />
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="">Area Oficina Sección</label>
+                        <select defaultValue="DEFAULT" onChange={handleChange} name="area_oficina_seccion_id" required className="form-select custom-select mr-sm-2">
+                          <option value="DEFAULT" disabled>--- Elegir Subunidad ---</option>
+
+                          {subunidades.map((objTipoFormato, i) => {
+                            return (
+                              <option key={objTipoFormato.id} value={objTipoFormato.id} >{objTipoFormato.nombre}</option>
+                            );
+                          })}
+                        </select>
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="">Personal_id</label>
+                        <select defaultValue="DEFAULT" onChange={handleChange} name="personal_id" required className="form-select custom-select mr-sm-2">
+                          <option value="DEFAULT" disabled>--- Elegir Personal ---</option>
+
+                          {personal.map((objPersonal, i) => {
+                            return (
+                              <option key={objPersonal.id} value={objPersonal.id} >{objPersonal.apellido}</option>
+                            );
+                          })}
+                        </select>
+                      </div>
+                      {/* <div className="form-group">
+                        <label htmlFor="">Tipo bien</label>
+                        <input type="text" className="form-control"
+                          value={tipo_bien} name="tipo_bien" onChange={handleChange} />
+                      </div> */}
+
+                      <div className="form-group">
+                        <button className="btn btn-primary" type="submit">ReasignarBien</button>
+                      </div>
+
+
+                    </form>
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                      Cerrar
+                    </Button>
+
+                  </Modal.Footer>
+                </Modal>
+
+
+
               </div>
             </div>
           </main>
