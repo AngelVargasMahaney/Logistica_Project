5309b705e7a57ca0d24cca38a2eef5ab439b4787
DESKTOP-FG0VTJH\usuario <kristianccahui@gmail.com>
2021-10-21 13:44:13 -0500

    Bienes Auxiliares: Historial editar y eliminar

--- src/modules/general/historiales/BienesAuxiliaresHistory.jsx
+++ src/modules/general/historiales/BienesAuxiliaresHistory.jsx
@@ -4,9 +4,12 @@ import GeneralNavBar from '../../layout/GeneralNavBar'
 
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { getHistorialBienAuxiliarById } from '../../../services/historialBienesService'
+import { deleteHistorialById, getHistorialBienAuxiliarById, postEditarHistorialById } from '../../../services/historialBienesService'
 import Modal from "react-bootstrap/Modal";
-
+import Swal from 'sweetalert2'
+import { Button } from 'react-bootstrap'
+import { getPersonalActivo } from '../../../services/personalService'
+import { getAreaOficinaSeccion } from '../../../services/areaOficinaSeccionService'
 const BienesAuxiliaresHistory = () => {
 
     const TITULO = "Bienes Auxiliares";
@@ -14,6 +17,12 @@ const BienesAuxiliaresHistory = () => {
     const [cargando, setCargando] = useState(true)
     const [pdfActual, setpdfActual] = useState("");
     const [isOpen, setIsOpen] = useState(false);
+    const [formulario, setFormulario] = useState({
+        personalEncargado: "",
+        areaOficinaSeccion: "",
+        estado_bien: "",
+        observaciones: "",
+    })
 
     const params = useParams()
     const traerData = () => {
@@ -36,6 +45,148 @@ const BienesAuxiliaresHistory = () => {
         // eslint-disable-next-line
     }, [])
     let { internamiento, historial } = data;
+
+
+    const eliminarHistorial = (idBien) => {
+        Swal.fire({
+            title: '¿Seguro que deseas eliminar este historial?',
+            icon: 'warning',
+            text: 'Los cambios serán irreversibles 😮',
+            showCancelButton: true
+        }).then((rpta) => {
+            if (rpta.isConfirmed) {
+                deleteHistorialById(idBien).then((rpta) => {
+                    if (rpta.status === 200) {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Eliminación Correcta',
+                            text: 'El historial fue eliminado de manera exitosa',
+                        })
+                        //Se comprueba que se eliminó correctamente
+                        traerData() //Se llama otra vez para setear la variable de estado y recargar la página automáticamente al borrar un usuario
+                    } else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Ocurrió un problema',
+                            text: 'El historial no se pudo eliminar',
+                        })
+                    }
+                })
+            }
+        })
+    }
+
+    const [idActualHistorialItem, setIdActualHistorialItem] = useState(null)
+    const [showModalHistorial, setShowModalHistorial] = useState(false)
+    const showModalEditarHistorial = (idBien, obj) => {
+        setIdActualHistorialItem(idBien);
+        setFormularioHistorial({ ...obj })
+        setShowModalHistorial(true);
+        console.log("ENTRANDO AL LLAMADO DE DATA CON ID: " + idBien)
+        // setCargando(true);
+        // getHistorialFormatoById(idBien).then(rpta => {
+        //   console.log("adwdwaw" + rpta)
+        //   setDataHistorial(rpta.data);
+        //   console.log("PRUEBAA" + rpta);
+        //   setCargando(false);
+        // })
+    }
+    const [formularioHistorial, setFormularioHistorial] = useState(({
+        personal_id: "",
+        area_oficina_seccion_id: "",
+        estado_del_bien: "",
+        observaciones: "",
+
+    }))
+    const handleCloseModalHistorial = () => setShowModalHistorial(false);
+    const handleChange = (e) => {
+        setFormularioHistorial({
+            ...formularioHistorial,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    
+    const token = localStorage.getItem('token')
+    const config = {
+        headers: {
+            'Content-Type': 'multipart/form-data',
+            'Authorization': `Bearer ${token}`
+        }
+    }
+    const [documentoEntregaRecepcion, setDocumentoEntregaRecepcion] = useState(null)
+    const handleDocumentoEntregaRecepcion = e => {
+        setDocumentoEntregaRecepcion(e.target.files[0])
+    }
+
+    const [documentoMemorandum, setDocumentoMemorandum] = useState(null)
+    const handleDocumentoMemorandum = e => {
+        setDocumentoMemorandum(e.target.files[0])
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('personal_id', formularioHistorial.personal_id)
+        formData.append('area_oficina_seccion_id', formularioHistorial.area_oficina_seccion_id)
+        formData.append('estado_del_bien', formularioHistorial.estado_del_bien)
+        formData.append('observaciones', formularioHistorial.observaciones)
+        if (documentoEntregaRecepcion !== null) {
+            formData.append('documento_acta_entrega_recepcion', documentoEntregaRecepcion)
+        } else {
+            formData.delete('documento_acta_entrega_recepcion', documentoEntregaRecepcion)
+        }
+        if (documentoMemorandum !== null) {
+            formData.append('documento_memorandum', documentoMemorandum)
+        } else {
+            formData.delete('documento_memorandum', documentoMemorandum)
+        }
+
+
+        postEditarHistorialById(formData, config, idActualHistorialItem).then((rpta) => {
+            if (rpta.status === 200) { //Si el status es OK, entonces redirecciono a la lista de usuarios
+                console.log("Datos actualizados correctamente")
+                Swal.fire(
+                    'Historial',
+                    'Se actualizó el historial correctamente',
+                    'success'
+                )
+                traerData()
+                setShowModalHistorial(false)
+            }
+        })
+    }
+
+    const [personalActivo, setPersonalActivo] = useState([]);
+    const traerPersonalActivo = () => {
+        setCargando(true)
+        getPersonalActivo().then((rpta) => {
+            //console.log(rpta);
+            setPersonalActivo(rpta.data);
+            setCargando(false)
+        });
+    };
+    useEffect(() => {
+        traerPersonalActivo();
+    }, []);
+
+    const [areaoficinaseccion, setAreaoficinaseccion] = useState([]);
+    const traerSubunidades = () => {
+        setCargando(true)
+        getAreaOficinaSeccion().then((rpta) => {
+
+            setAreaoficinaseccion(rpta.data);
+            setCargando(false)
+        }).catch((err) => {
+            console.log("Data no cargada en traerSubunidades")
+        })
+
+    };
+    useEffect(() => {
+        traerSubunidades();
+    }, []);
+    let { personal,area_oficina_seccion } = formularioHistorial
+   
 
     return (
         <>
@@ -136,6 +287,24 @@ const BienesAuxiliaresHistory = () => {
                                                         <span className="badge badge-pill badge-secondary">{j - i}</span>
                                                         {i === 0 && data.is_internado === false ? (<><span className="badge badge-pill badge-primary ml-1">UBICACIÓN ACTUAL</span></>) : (<></>)}
 
+                                                        <button
+                                                            type="button"
+                                                            class="btn pull-right mx-3 btn-outline-danger"
+                                                            onClick={() => {
+                                                                eliminarHistorial(item.id);
+                                                            }}
+                                                        >Eliminar
+
+                                                        </button>
+                                                        <button
+                                                            type="button"
+                                                            class="btn pull-right mx-3 btn-outline-info"
+                                                            onClick={() => {
+                                                                showModalEditarHistorial(item.id, item);
+                                                            }}
+                                                        >Editar Historial
+
+                                                        </button>
                                                         <div className="mt-1">Fecha: {item?.fecha} </div>
                                                         <div className="mt-1">Personal encargado: {personal?.grado} {personal?.nombre} {personal?.apellido} </div>
                                                         <div className="mt-1">Subunidad: {area_oficina_seccion?.subunidad?.nombre}</div>
@@ -200,6 +369,118 @@ const BienesAuxiliaresHistory = () => {
                 </Modal>
 
             </div>
+            <Modal show={showModalHistorial} onHide={handleCloseModalHistorial}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Edición de un historial</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <form onSubmit={handleSubmit}>
+                        {/* <div className="form-group">
+                            <label htmlFor="">Fecha:</label>
+                            <input type="date" className="form-control"
+                                value={fecha} name="fecha" onChange={handleChange} />
+                        </div> */}
+                        <div className="form-group">
+                            <label htmlFor="">Nueva persona encargada</label>
+                            <select defaultValue="DEFAULT" onChange={handleChange} name="personal_id" required className="form-select custom-select mr-sm-2">
+                                <option value="DEFAULT" disabled>{personal?.grado +  " |-> " + personal?.apellido + " " + personal?.nombre}</option>
+
+                                {personalActivo.map((objPersonal, i) => {
+                                    return (
+                                        <option key={objPersonal.id} value={objPersonal.id} >{objPersonal.grado + " |-> " + objPersonal.apellido + " " + objPersonal.nombre} {formulario.personalEncargado = objPersonal.apellido}</option>
+                                    );
+                                })}
+                            </select>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="">Area Oficina Sección</label>
+                            <select defaultValue="DEFAULT" onChange={handleChange} name="area_oficina_seccion_id" required className="form-select custom-select mr-sm-2">
+                                
+                                <option value="DEFAULT" disabled>{area_oficina_seccion?.nombre + " |-> " + area_oficina_seccion?.subunidad?.nombre }</option>
+                                {areaoficinaseccion.map((objTipoFormato, i) => {
+                                    let { subunidad } = objTipoFormato
+                                    return (
+                                        <option key={objTipoFormato.id} value={objTipoFormato.id}>{objTipoFormato.nombre + " |-> " + subunidad.nombre}</option>
+
+                                    );
+                                })}
+
+                            </select>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="">Estado del bien: </label>
+                            <input type="text" className="form-control"
+                                value={formularioHistorial.estado_del_bien}
+                                name="estado_del_bien" onChange={handleChange} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="">Observaciones: </label>
+                            <input type="text" className="form-control"
+                                value={formularioHistorial.observaciones}
+                                name="observaciones" onChange={handleChange} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="">Documento Acta Entrega Recepción: </label>
+                            <input type="file" className="form-control"
+                                name="documento_acta_entrega_recepcion" onChange={handleDocumentoEntregaRecepcion} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="">Documento Memorandum: </label>
+                            <input type="file" className="form-control"
+                                name="documento_memorandum" onChange={handleDocumentoMemorandum} />
+                        </div>
+                        {/* <div className="form-group">
+                            <label htmlFor="">SubUnidad:</label>
+                            <input type="text" className="form-control"
+                                value={sub_unidad} name="estado_del_bien" onChange={handleChange} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="">Area:</label>
+                            <input type="text" className="form-control"
+                                value={area} name="estado_del_bien" onChange={handleChange} />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="">Estado del Bien:</label>
+                            <input type="text" className="form-control"
+                                value={estado_del_bien} name="estado_del_bien" onChange={handleChange} />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="">Observaciones:</label>
+                            <input type="text" className="form-control"
+                                value={observaciones} name="observaciones" onChange={handleChange} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="">Documento-Acta entrega y recepción:</label>
+                            <input type="file" className="form-control"
+                                name="documento_acta_entrega_recepcion" onChange={handleDocumentRecepcion} />
+                        </div>
+                        <div className="form-group" hidden>
+                            <label htmlFor="">Id del Bien:</label>
+                            <input type="text" className="form-control"
+                                value={idActualDelBien} name="bien_id" onChange={handleChange} />
+                        </div> */}
+                        {/* <div className="form-group">
+                        <label htmlFor="">Tipo bien</label>
+                        <input type="text" className="form-control"
+                          value={tipo_bien} name="tipo_bien" onChange={handleChange} />
+                      </div> */}
+
+                        <div className="form-group">
+                            <button className="btn btn-primary" type="submit">Actualizar <i className="ml-2 fa fa-check"></i></button>
+                        </div>
+
+
+                    </form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseModalHistorial}>
+                        Cerrar
+                    </Button>
+
+                </Modal.Footer>
+            </Modal>
 
 
 
